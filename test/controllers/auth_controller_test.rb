9f8ac466719fc09d52d95beea9415cbba8a21a04
Ricardo Rivas <richistron@gmail.com>
2020-12-16 11:40:40 -0600

    tests

--- test/controllers/auth_controller_test.rb
+++ test/controllers/auth_controller_test.rb
@@ -24,4 +24,16 @@ class AuthControllerTest < ActionDispatch::IntegrationTest
     post me_url, headers: { 'Authorization' => 'Token nope' }
     assert_response :unauthorized
   end
+
+  test 'login' do
+    user = User.create! email: 'test@mail.com', password: '123'
+    user_token = UserToken.create! user: user
+    post login_url,
+         headers: { 'Authorization' => 'Token 123' },
+         params: { email: user.email, password: '123' }
+    assert_response :success
+    assert user_token.token
+    json_response = JSON.parse response.body
+    assert_equal user_token.token, json_response['user']['access_token']
+  end
 end
